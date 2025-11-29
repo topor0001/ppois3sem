0a6fd7fa7973e58749a56a6f39d65f20b7462088
topor0001 <zhdan0616@gmail.com>
2025-11-29 03:20:45 +0300

    Add files via upload

---
+++ lab1/set.py
@@ -0,0 +1,254 @@
+class Set:
+    MAX_NESTING_LEVEL = 100
+    _set_counter = 0
+    
+    def __init__(self, elements=None, unique_id=None):
+        Set._set_counter += 1
+        self._elements = []
+        self._id = unique_id or Set._set_counter
+        if elements is not None:
+            for element in elements:
+                self.add_element(element)
+    
+    def _contains_element(self, element):
+        for elem in self._elements:
+            if self._elements_equal(elem, element):
+                return True
+        return False
+    
+    def _elements_equal(self, elem1, elem2):
+        if isinstance(elem1, Set) and isinstance(elem2, Set):
+            return elem1._id == elem2._id
+        else:
+            return elem1 == elem2
+    
+    def is_empty(self):
+        return len(self._elements) == 0
+    
+    def add_element(self, element):
+        if not self._contains_element(element):
+            self._elements.append(element)
+    
+    def remove_element(self, element):
+        new_elements = []
+        for elem in self._elements:
+            if not self._elements_equal(elem, element):
+                new_elements.append(elem)
+        self._elements = new_elements
+    
+    def get_cardinality(self):
+        return len(self._elements)
+    
+    def __contains__(self, element):
+        return self._contains_element(element)
+    
+    def __getitem__(self, element):
+        return self._contains_element(element)
+    
+    def __add__(self, other):
+        if not isinstance(other, Set):
+            return NotImplemented
+        
+        result = Set()
+        for elem in self._elements:
+            result.add_element(elem)
+        for elem in other._elements:
+            result.add_element(elem)
+        return result
+    
+    def __iadd__(self, other):
+        if not isinstance(other, Set):
+            return NotImplemented
+        for elem in other._elements:
+            self.add_element(elem)
+        return self
+    
+    def __mul__(self, other):
+        if not isinstance(other, Set):
+            return NotImplemented
+        result = Set()
+        for elem in self._elements:
+            if elem in other:
+                result.add_element(elem)
+        return result
+    
+    def __imul__(self, other):
+        if not isinstance(other, Set):
+            return NotImplemented
+        new_elements = []
+        for elem in self._elements:
+            if elem in other:
+                new_elements.append(elem)
+        self._elements = new_elements
+        return self
+    
+    def __sub__(self, other):
+        if not isinstance(other, Set):
+            return NotImplemented
+        result = Set()
+        for elem in self._elements:
+            if elem not in other:
+                result.add_element(elem)
+        return result
+    
+    def __isub__(self, other):
+        if not isinstance(other, Set):
+            return NotImplemented
+        new_elements = []
+        for elem in self._elements:
+            if elem not in other:
+                new_elements.append(elem)
+        self._elements = new_elements
+        return self
+    
+    def get_power_set(self):
+        elements_list = self._elements
+        n = len(elements_list)
+        total_subsets = 1 << n  
+        all_subsets = []
+        for i in range(total_subsets):
+            subset = Set(unique_id=f"subset_{i}")
+            for j in range(n):
+                if i & (1 << j):
+                    subset.add_element(elements_list[j])
+            all_subsets.append(subset)
+        power_set = Set(unique_id="power_set")
+        for subset in all_subsets:
+            power_set.add_element(subset)
+        
+        return power_set
+    
+    def __eq__(self, other):
+        if not isinstance(other, Set):
+            return NotImplemented
+        if len(self._elements) != len(other._elements):
+            return False
+        for elem in self._elements:
+            if not other._contains_element(elem):
+                return False
+        return True
+    
+    def __ne__(self, other):
+        return not self == other
+    
+    def __str__(self):
+        if self.is_empty():
+            return "{}"
+        elements_str = []
+        for elem in self._elements:
+            if isinstance(elem, Set):
+                elements_str.append(str(elem))
+            else:
+                elements_str.append(repr(elem)) 
+        return "{" + ", ".join(elements_str) + "}"
+    
+    def __repr__(self):
+        return f"Set(elements={len(self._elements)})"
+    
+    def __iter__(self):
+        return _SetIterator(self._elements)
+    
+    def __len__(self):
+        return len(self._elements)
+    
+    @classmethod
+    def from_string(cls, string_repr):
+        return cls._parse_from_string(string_repr.strip())
+    
+    @classmethod
+    def _parse_from_string(cls, string_repr, level=0):
+        if level > cls.MAX_NESTING_LEVEL:
+            raise ValueError("Maximum nesting level exceeded")
+        
+        string_repr = string_repr.strip()
+        if not string_repr.startswith('{') or not string_repr.endswith('}'):
+            raise ValueError("Set must be enclosed in curly braces")
+        content = string_repr[1:-1].strip()
+        if not content:
+            return cls() 
+        
+        result = cls()
+        pos = 0
+        brace_count = 0  
+        current_element = [] 
+        element_counter = 0 
+        
+        while pos < len(content):
+            char = content[pos]
+            
+            if char == '{':
+                brace_count += 1
+                current_element.append(char)
+            elif char == '}':
+                brace_count -= 1
+                current_element.append(char)
+            elif char == ',' and brace_count == 0:
+                element_str = cls._chars_to_string(current_element).strip()
+                if element_str:
+                    if element_str.startswith('{'):
+                        nested_set = cls._parse_from_string(element_str, level + 1)
+                        result.add_element(nested_set)
+                    else:
+                        result.add_element(element_str.strip())
+                        element_counter += 1
+                current_element = []
+            else:
+                current_element.append(char)
+            
+            pos += 1
+        
+        if current_element:
+            element_str = cls._chars_to_string(current_element).strip()
+            if element_str:
+                if element_str.startswith('{'):
+                    nested_set = cls._parse_from_string(element_str, level + 1)
+                    result.add_element(nested_set)
+                else:
+                    result.add_element(element_str.strip())
+        
+        return result
+    
+    @staticmethod
+    def _chars_to_string(chars):
+        result = ""
+        for char in chars:
+            result += char
+        return result
+
+    def copy(self):
+        new_set = Set()
+        for element in self._elements:
+            if isinstance(element, Set):
+                copied_element = Set(unique_id=element._id)
+                for sub_elem in element._elements:
+                    copied_element.add_element(sub_elem)
+                new_set.add_element(copied_element)
+            else:
+                new_set.add_element(element)
+        return new_set
+    
+    def __copy__(self):
+        return self.copy()
+    
+    def __deepcopy__(self, memo):
+        return self.copy()
+    
+    def __del__(self):
+        Set._set_counter -= 1
+
+
+class _SetIterator:
+    
+    def __init__(self, elements):
+        self._elements = elements
+        self._index = 0
+    
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        if self._index < len(self._elements):
+            result = self._elements[self._index]
+            self._index += 1
+            return result
+        raise StopIteration
---
+++ lab1/test_set.py
@@ -0,0 +1,348 @@
+import unittest
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from set import Set
+
+def create_unique_set():
+    return Set()
+
+class TestSet(unittest.TestCase):
+    def test_default_constructor(self):
+        test_set = Set()
+        self.assertTrue(test_set.is_empty())
+        self.assertEqual(test_set.get_cardinality(), 0)
+    
+    def test_constructor_with_elements(self):
+        element1 = create_unique_set()
+        element2 = create_unique_set()
+        test_set = Set([element1, element2])
+        self.assertEqual(test_set.get_cardinality(), 2)
+    
+    def test_copy_functionality(self):
+        set1 = Set()
+        element = create_unique_set()
+        set1.add_element(element)
+        
+        set2 = set1.copy()
+        self.assertEqual(set1, set2)
+        self.assertIsNot(set1, set2)
+    
+    def test_add_remove_elements(self):
+        test_set = Set()
+        element1 = create_unique_set()
+        element2 = create_unique_set()
+        
+        test_set.add_element(element1)
+        self.assertEqual(test_set.get_cardinality(), 1)
+        
+        test_set.add_element(element2)
+        self.assertEqual(test_set.get_cardinality(), 2)
+        
+        test_set.remove_element(element1)
+        self.assertEqual(test_set.get_cardinality(), 1)
+    
+    def test_add_duplicate_elements(self):
+        test_set = Set()
+        element = create_unique_set()
+        
+        test_set.add_element(element)
+        test_set.add_element(element)  
+        self.assertEqual(test_set.get_cardinality(), 1)
+    
+    def test_contains_operator(self):
+        test_set = Set()
+        element = create_unique_set()
+        
+        self.assertNotIn(element, test_set)
+        test_set.add_element(element)
+        self.assertIn(element, test_set)
+    
+    def test_element_access_operator(self):
+        test_set = Set()
+        element = create_unique_set()
+        
+        self.assertFalse(test_set[element])
+        test_set.add_element(element)
+        self.assertTrue(test_set[element])
+    
+    def test_union_operation(self):
+        set1 = Set()
+        set2 = Set()
+        element1 = create_unique_set()
+        element2 = create_unique_set()
+        
+        set1.add_element(element1)
+        set2.add_element(element2)
+        
+        union_set = set1 + set2
+        self.assertEqual(union_set.get_cardinality(), 2)
+    
+    def test_intersection_operation(self):
+        set1 = Set()
+        set2 = Set()
+        common_element = create_unique_set()
+        
+        set1.add_element(common_element)
+        set2.add_element(common_element)
+        
+        intersection_set = set1 * set2
+        self.assertEqual(intersection_set.get_cardinality(), 1)
+    
+    def test_empty_intersection(self):
+        set1 = Set()
+        set2 = Set()
+        element1 = create_unique_set()
+        element2 = create_unique_set()
+        
+        set1.add_element(element1)
+        set2.add_element(element2)
+        
+        intersection_set = set1 * set2
+        self.assertEqual(intersection_set.get_cardinality(), 0)
+    
+    def test_difference_operation(self):
+        set1 = Set()
+        set2 = Set()
+        element1 = create_unique_set()
+        element2 = create_unique_set()
+        
+        set1.add_element(element1)
+        set1.add_element(element2)
+        set2.add_element(element1)
+        
+        difference_set = set1 - set2
+        self.assertEqual(difference_set.get_cardinality(), 1)
+    
+    def test_in_place_operations(self):
+        set1 = Set()
+        set2 = Set()
+        element1 = create_unique_set()
+        element2 = create_unique_set()
+        
+        set1.add_element(element1)
+        set2.add_element(element2)
+        
+        set1 += set2
+        self.assertEqual(set1.get_cardinality(), 2)
+        
+        intersection_set = Set([element2])
+        set1 *= intersection_set
+        self.assertEqual(set1.get_cardinality(), 1)
+        
+        difference_set = Set([element2])
+        set1 -= difference_set
+        self.assertEqual(set1.get_cardinality(), 0)
+    
+    def test_power_set_empty(self):
+        test_set = Set()
+        power_set = test_set.get_power_set()
+        self.assertEqual(power_set.get_cardinality(), 1) 
+    
+    def test_power_set_one_element(self):
+        test_set = Set()
+        element = create_unique_set()
+        test_set.add_element(element)
+        
+        power_set = test_set.get_power_set()
+        self.assertEqual(power_set.get_cardinality(), 2)  
+    
+    def test_power_set_two_elements(self):
+        test_set = Set()
+        element1 = create_unique_set()
+        element2 = create_unique_set()
+        
+        test_set.add_element(element1)
+        test_set.add_element(element2)
+        
+        power_set = test_set.get_power_set()
+        self.assertEqual(power_set.get_cardinality(), 4) 
+    
+    def test_comparison_operators(self):
+        set1 = Set()
+        set2 = Set()
+        element = create_unique_set()
+        
+        set1.add_element(element)
+        set2.add_element(element)
+        
+        self.assertEqual(set1, set2)
+        
+        set3 = Set()
+        different_element = create_unique_set()
+        set3.add_element(different_element)
+        self.assertNotEqual(set1, set3)
+    
+    def test_string_representation(self):
+        test_set = Set()
+        self.assertEqual(str(test_set), "{}")
+        
+        element = create_unique_set()
+        test_set.add_element(element)
+        self.assertTrue(str(test_set).startswith("{"))
+        self.assertTrue(str(test_set).endswith("}"))
+    
+    def test_from_string_empty(self):
+        test_set = Set.from_string("{}")
+        self.assertTrue(test_set.is_empty())
+    
+    def test_iterator(self):
+        test_set = Set()
+        element1 = create_unique_set()
+        element2 = create_unique_set()
+        
+        test_set.add_element(element1)
+        test_set.add_element(element2)
+        
+        elements = list(test_set)
+        self.assertEqual(len(elements), 2)
+    
+    def test_length_operator(self):
+        test_set = Set()
+        self.assertEqual(len(test_set), 0)
+        
+        test_set.add_element(create_unique_set())
+        self.assertEqual(len(test_set), 1)
+
+
+class TestSetAdvanced(unittest.TestCase):
+    
+    def test_nested_sets(self):
+        inner_set = Set()
+        element = create_unique_set()
+        inner_set.add_element(element)
+        
+        outer_set = Set()
+        outer_set.add_element(inner_set)
+        
+        self.assertEqual(outer_set.get_cardinality(), 1)
+        self.assertTrue(inner_set in outer_set)
+    
+    def test_complex_operations(self):
+        set1 = Set()
+        set2 = Set()
+        set3 = Set()
+        
+        elem1, elem2, elem3 = create_unique_set(), create_unique_set(), create_unique_set()
+        
+        set1.add_element(elem1)
+        set1.add_element(elem2)
+        set2.add_element(elem2)
+        set2.add_element(elem3)
+        set3.add_element(elem1)
+        
+        result = (set1 + set2) * set3
+        self.assertEqual(result.get_cardinality(), 1)
+        self.assertTrue(elem1 in result)
+    
+    def test_power_set_properties(self):
+        test_set = Set()
+        elem1, elem2 = create_unique_set(), create_unique_set()
+        test_set.add_element(elem1)
+        test_set.add_element(elem2)
+        
+        power_set = test_set.get_power_set()
+        
+        empty_set_exists = False
+        for subset in power_set:
+            if subset.is_empty():
+                empty_set_exists = True
+                break
+        self.assertTrue(empty_set_exists)
+        
+        original_set_exists = False
+        for subset in power_set:
+            if subset == test_set:
+                original_set_exists = True
+                break
+        self.assertTrue(original_set_exists)
+    
+    def test_set_identity(self):
+        set1 = Set([create_unique_set(), create_unique_set()])
+        empty_set = Set()
+        
+        union_with_empty = set1 + empty_set
+        self.assertEqual(union_with_empty, set1)
+        
+        intersection_with_empty = set1 * empty_set
+        self.assertEqual(intersection_with_empty, empty_set)
+
+        difference_with_empty = set1 - empty_set
+        self.assertEqual(difference_with_empty, set1)
+
+def test_large_set_operations(self):
+    set1 = Set()
+
+    elements1 = [create_unique_set() for _ in range(3)]
+    for elem in elements1:
+        set1.add_element(elem)
+    
+    set2 = Set()
+
+    elements2 = [create_unique_set() for _ in range(2)]
+    for elem in elements2:
+        set2.add_element(elem)
+    
+    union_set = set1 + set2
+
+    self.assertEqual(union_set.get_cardinality(), 5)
+
+class TestSetEdgeCases(unittest.TestCase):
+    
+    def test_self_operations(self):
+        test_set = Set([create_unique_set(), create_unique_set()])
+        
+        union_with_self = test_set + test_set
+        self.assertEqual(union_with_self, test_set)
+
+        intersection_with_self = test_set * test_set
+        self.assertEqual(intersection_with_self, test_set)
+
+        difference_with_self = test_set - test_set
+        self.assertTrue(difference_with_self.is_empty())
+    
+    def test_multiple_nesting(self):
+        level1 = Set()
+        level2 = Set()
+        level3 = Set()
+        
+        element = create_unique_set()
+        level3.add_element(element)
+        level2.add_element(level3)
+        level1.add_element(level2)
+        
+        self.assertEqual(level1.get_cardinality(), 1)
+        self.assertEqual(level2.get_cardinality(), 1)
+        self.assertEqual(level3.get_cardinality(), 1)
+    
+    def test_power_set_of_power_set(self):
+        original_set = Set([create_unique_set(), create_unique_set()])
+        power_set = original_set.get_power_set()
+        power_set_of_power_set = power_set.get_power_set()
+
+        self.assertEqual(power_set_of_power_set.get_cardinality(), 16)
+    
+    def test_remove_nonexistent_element(self):
+        test_set = Set([create_unique_set(), create_unique_set()])
+        original_cardinality = test_set.get_cardinality()
+        
+        nonexistent_element = create_unique_set()
+        test_set.remove_element(nonexistent_element)
+
+        self.assertEqual(test_set.get_cardinality(), original_cardinality)
+    
+    def test_empty_set_operations(self):
+        empty1 = Set()
+        empty2 = Set()
+        non_empty = Set([create_unique_set()])
+        
+        self.assertEqual(empty1 + empty2, empty1)
+
+        self.assertEqual(empty1 * non_empty, empty1)
+
+        self.assertEqual(non_empty - empty1, non_empty)
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
